e9833d74cc1f735a62134fe617730354f2cd38e9
ZzzGayane <aleksanyang143@gmail.com>
2026-05-11 10:55:26 +0400

    Update teacher.py

--- library-system/src/models/teacher.py
+++ library-system/src/models/teacher.py
@@ -1,1 +1,14 @@
+from .person import Person
 
+class Teacher(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def assign_grade(self, student, course, grade):
+        student.grades[course.name] = grade
+
+    def display_info(self):
+        return f"Teacher: {self.name}, Age: {self.age}"
+
+    def get_role(self):
+        return "Teacher"
