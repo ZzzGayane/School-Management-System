67fbc6c010396d85b1251c46a35178211dd7768d
ZzzGayane <aleksanyang143@gmail.com>
2026-05-11 10:54:42 +0400

    Update student.py

--- library-system/src/models/student.py
+++ library-system/src/models/student.py
@@ -1,9 +1,18 @@
 from .person import Person
 
 class Student(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.courses = []
+        self.grades = {}
+
+    def enroll(self, course):
+        if course not in self.courses:
+            self.courses.append(course)
+            course.add_student(self)
 
     def display_info(self):
-        return "Student"
+        return f"Student: {self.name}, Age: {self.age}"
 
     def get_role(self):
         return "Student"
